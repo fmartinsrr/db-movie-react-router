190876ec06ab445c3708e971e29267a1d3c4ca51
Filipe Martins <>
2023-01-31 14:38:38 +0000

    update movie details screen with more content :D

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -14,11 +14,25 @@ export default function Results() {
   const navigate = useNavigate();
 
   return (
-    <div className="container mt-6">
-      <p className="title is-4 mt-6">{details.title}</p>
+    <div className="container mt-6 mx-6">
       <button type="button" onClick={() => {
             navigate(-1);
         }}>Back</button>
+
+      <div className="level mt-5">
+        <div className="level-left">
+          <div className="level-item">
+            <figure className="image">
+              <img src={ TMDB.getFullAssetUrl(details.backdrop_path) } alt="Placeholder image" />
+            </figure>
+          </div>
+        </div>
+      </div>
+
+      <p className="title is-3 mt-3">{details.title}</p>
+      <p><b>Release date: </b>{ details.release_date}</p>
+      <p className="mt-3">{ details.overview }</p>
+      
     </div>
   )
 }
