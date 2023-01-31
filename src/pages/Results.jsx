fd3ad7c1963bf322fd8f98404f1c65f402b03c39
Filipe Martins <>
2023-01-31 10:30:56 +0000

    use the use effect to update the input from the url

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,31 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Form, useLoaderData, useNavigation } from "react-router-dom";
 import { TMDB } from '../helpers/TMDB'
 import { MovieCard } from "../components/MovieCard";
 
 export async function loader({ request, params }) {
   const url = new URL(request.url);
-  const query = url.searchParams.get("search");
-  const response = await TMDB.searchMovies(query);
+  const search = url.searchParams.get("search");
+  const response = await TMDB.searchMovies(search);
   const status = response.status;
   const movies = (status === 200 ? response.data.results : []);
-  return { movies, status, query };
+  return { movies, status, search };
 }
 
 export default function Results() {
-  const { movies, status, query } = useLoaderData();
-  const navigation = useNavigation();
-
-  const searching =
-    navigation.location &&
-    new URLSearchParams(navigation.location.search).has(
-      "search"
-    );
-
+  const { movies, status, search } = useLoaderData();
+  const [ query, setQuery] = useState("");
+  
   useEffect(() => {
-    document.getElementById("search").value = query;
-  }, [query]);
-
+    setQuery(search);
+  }, [search])
+  
   return (
     <div className="container mt-6">
       <Form onSubmit={ (event) => {
@@ -35,7 +29,7 @@ export default function Results() {
       }}>
         <div className="level">
           <div className="level-item">
-            <input id="search" name="search" className="input mr-1" type="text" placeholder="Find a movie" />
+            <input name="search" className="input mr-1" type="text" placeholder="Find a movie" value={query} onChange={(e) => setQuery(e.target.value) }/>
             <button className="button is-success">Search</button>
           </div>
         </div>
