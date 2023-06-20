9f89b89e94a80d2ca2bbf526227abd0ed33146c7
Filipe Martins <>
2023-06-20 17:50:06 +0100

    step 12: cache all website (not advised this way, but gives an ideia what can be done)

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
+const OFFLINE_VERSION = 2;
 
 // By including the favicon we will prevent an error for this asset.
 const PRE_CACHED_RESOURCES = [
@@ -9,7 +9,7 @@ const PRE_CACHED_RESOURCES = [
   "offline.css"
 ];
 
-const CACHE_NAME = "offline_v1";
+const CACHE_NAME = "offline_v2";
 
 // This code executes in its own worker or thread
 self.addEventListener("install", event => {
@@ -50,17 +50,25 @@ self.addEventListener("activate", event => {
 // By design, without any return the service worker will fallback to the default behaviour.
 
 self.addEventListener("fetch", event => {
-	console.log("Service worker fetch - Serving offline page");
+	console.log("Service worker fetch - Serve response, or cache, or offline page");
   async function navigateOrDisplayOfflinePage() {
+    const cache = await caches.open(CACHE_NAME);
     try {
       // Try to load the page from the network.
       const networkResponse = await fetch(event.request);
+
+      // It's important to create a clone of the request before caching it.
+      // This is because the original request object can only be consumed once.
+      // Once it is used by the browser or service worker, it becomes unusable.
+      const responseClone = networkResponse.clone();
+      cache.put(event.request, responseClone);
+
       return networkResponse;
     } catch (error) {
       // The network call failed, the device is offline.
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match("offline.html");
-      return cachedResponse;
+      const cachedResponse = await cache.match(event.request);
+      // Return an already cached resource or show the offline page.
+      return cachedResponse || await cache.match("offline.html");
     }
   }
 
@@ -74,16 +82,33 @@ self.addEventListener("fetch", event => {
 
 self.addEventListener("fetch", event => {
   console.log("Service worker fetch - Cache strategy - Cache first");
-  async function serveCacheThenFetch() {
+  async function fetchAndCacheOrReturnCache() {
     const cache = await caches.open(CACHE_NAME);
     const cachedResponse = await cache.match(event.request);
-    return cachedResponse || await fetch(event.request);
+    try {
+      const networkResponse = await fetch(event.request);
+			
+      // It's important to create a clone of the request before caching it.
+      // This is because the original request object can only be consumed once.
+      // Once it is used by the browser or service worker, it becomes unusable.
+      const responseClone = networkResponse.clone();
+      cache.put(event.request, responseClone);
+      
+      // Option 1: Prioritize cached response over network.
+      // return cachedResponse || networkResponse;
+      
+      // Option 2: allays return the fetched response.
+      return networkResponse;
+    }
+    catch (error) {
+	    return cachedResponse;
+    }
   }
 	
   const url = new URL(event.request.url);
   // For any resource we will try to return cached before fetch.
   if (event.request.mode !== 'navigate' && url.host !== 'api.themoviedb.org') {
-    event.respondWith(serveCacheThenFetch());
+    event.respondWith(fetchAndCacheOrReturnCache());
   }
 });
 
